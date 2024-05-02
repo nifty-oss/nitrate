523ec0a0ed0505e0edfae324f77269afaf1e00d9
febo <febo@tensor.so>
2024-05-02 13:14:15 +0100

    Moved to program crate

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,5 +1,5 @@
-// Copyright 2024 nifty-oss maintainers
-// Copyright 2024 Magnetar Fields
+// Copyright (c) 2024 nifty-oss maintainers
+// Copyright (c) 2024 Magnetar Fields
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
