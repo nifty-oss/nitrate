994322d830e9649ea080ec9e126826e6573379db
febo <febo@nifty-oss.org>
2024-07-04 21:14:01 +0100

    Fix support for empty signer

--- program/src/system.rs
+++ program/src/system.rs
@@ -38,7 +38,7 @@ pub fn create_account(
     space: u64,
     owner: &Pubkey,
 ) {
-    create_account_signed(funder, account, lamports, space, owner, &[])
+    _create_account_signed::<0>(funder, account, lamports, space, owner, &[]);
 }
 
 /// Create a new account with a program signed instruction.
@@ -59,31 +59,6 @@ pub fn create_account_signed<const SEEDS: usize>(
     owner: &Pubkey,
     signer_seeds: &[&[u8]; SEEDS],
 ) {
-    let mut instruction_accounts: [CAccountMeta; 2] = [funder.into(), account.into()];
-    // account being created is always a signer
-    instruction_accounts[1].is_signer = true;
-
-    // -   0..4: instruction discriminator
-    // -  4..12: lamports
-    // - 12..20: account space
-    // - 20..52: owner pubkey
-    let mut instruction_data = [0; 52];
-    // create account instruction has a '0' discriminator
-    instruction_data[4..12].copy_from_slice(&lamports.to_le_bytes());
-    instruction_data[12..20].copy_from_slice(&space.to_le_bytes());
-    instruction_data[20..52].copy_from_slice(owner.as_ref());
-
-    let instruction = CInstruction {
-        program_id: &system_program::ID,
-        accounts: instruction_accounts.as_ptr(),
-        accounts_len: instruction_accounts.len() as u64,
-        data: instruction_data.as_ptr(),
-        data_len: instruction_data.len() as u64,
-    };
-
-    // account infos and seeds
-    let account_infos: [CAccountInfo; 2] = [funder.into(), account.into()];
-
     // signer seeds
     let mut seeds: [std::mem::MaybeUninit<CSignerSeed>; SEEDS] =
         unsafe { std::mem::MaybeUninit::uninit().assume_init() };
@@ -100,20 +75,7 @@ pub fn create_account_signed<const SEEDS: usize>(
         len: SEEDS as u64,
     }];
 
-    #[cfg(target_os = "solana")]
-    unsafe {
-        solana_program::syscalls::sol_invoke_signed_c(
-            &instruction as *const CInstruction as *const u8,
-            account_infos.as_ptr() as *const u8,
-            account_infos.len() as u64,
-            signer.as_ptr() as *const u8,
-            signer.len() as u64,
-        );
-    }
-
-    // keep clippy happy
-    #[cfg(not(target_os = "solana"))]
-    core::hint::black_box(&(&instruction, &account_infos, &signer));
+    _create_account_signed::<SEEDS>(funder, account, lamports, space, owner, &signer);
 }
 
 /// Transfer lamports between accounts.
@@ -159,4 +121,68 @@ pub fn transfer(from: &AccountInfo, recipient: &AccountInfo, amount: u64) {
     // keep clippy happy
     #[cfg(not(target_os = "solana"))]
     core::hint::black_box(&(&instruction, &account_infos, &seeds));
+}
+
+//-- Internal functions
+
+/// Create a new account.
+///
+/// This function is used to create a new account either with or without a program
+/// signed instruction.
+///
+/// # Arguments
+///
+/// * `funder`: Funding account.
+/// * `account`: New account.
+/// * `lamports`: Number of lamports to transfer to the new account.
+/// * `space`: Number of bytes of memory to allocate.
+/// * `owner`: Address of program that will own the new account.
+/// * `signer`: Seeds used to sign the instruction.
+fn _create_account_signed<const SEEDS: usize>(
+    funder: &AccountInfo,
+    account: &AccountInfo,
+    lamports: u64,
+    space: u64,
+    owner: &Pubkey,
+    signer: &[CSigner],
+) {
+    let mut instruction_accounts: [CAccountMeta; 2] = [funder.into(), account.into()];
+    // account being created is always a signer
+    instruction_accounts[1].is_signer = true;
+
+    // -   0..4: instruction discriminator
+    // -  4..12: lamports
+    // - 12..20: account space
+    // - 20..52: owner pubkey
+    let mut instruction_data = [0; 52];
+    // create account instruction has a '0' discriminator
+    instruction_data[4..12].copy_from_slice(&lamports.to_le_bytes());
+    instruction_data[12..20].copy_from_slice(&space.to_le_bytes());
+    instruction_data[20..52].copy_from_slice(owner.as_ref());
+
+    let instruction = CInstruction {
+        program_id: &system_program::ID,
+        accounts: instruction_accounts.as_ptr(),
+        accounts_len: instruction_accounts.len() as u64,
+        data: instruction_data.as_ptr(),
+        data_len: instruction_data.len() as u64,
+    };
+
+    // account infos
+    let account_infos: [CAccountInfo; 2] = [funder.into(), account.into()];
+
+    #[cfg(target_os = "solana")]
+    unsafe {
+        solana_program::syscalls::sol_invoke_signed_c(
+            &instruction as *const CInstruction as *const u8,
+            account_infos.as_ptr() as *const u8,
+            account_infos.len() as u64,
+            signer.as_ptr() as *const u8,
+            signer.len() as u64,
+        );
+    }
+
+    // keep clippy happy
+    #[cfg(not(target_os = "solana"))]
+    core::hint::black_box(&(&instruction, &account_infos, &signer));
 }
